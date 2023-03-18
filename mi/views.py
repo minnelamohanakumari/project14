162b69a2f35c62eda7ad1bd36842afd9e547122b
minnelamohanakumari <minnelamohanakumari18gmail.com>
2023-03-18 13:05:22 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+def rohit(request):
+    return render(request,'rohit.html')
